00e4a6d19208b47e2ec14336ae94f49e1fbd7da0
Sonia Ndonga <soniandonga@yahoo.com>
2020-10-18 20:32:20 +0100

    Update Weather.js code

--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,9 @@
 import React from "react";
 import axios from "axios";
-import Loader from "react-loader-spinner";
 
 export default function Weather(props) {
-  function handleResponse(response) {
-    // alert(
-    //   `The weather in ${response.data.name} is ${response.data.main.temp}˚C`
-    // );
-  }
-  let apiKey = "cf23fefe944409418faf6ab205d2379d";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=
-  ${props.city}&appid=${apiKey}&units=${units}`;
-
+  function handleResponse() {
+   
   axios.get(apiUrl).then(handleResponse);
   return (
     <div className="Weather">
